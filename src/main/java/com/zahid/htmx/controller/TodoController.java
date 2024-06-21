89de1e429705484b37b24f54cfeffb0973f36608
digital-animal <zahidulislamssf@gmail.com>
2024-06-21 22:00:33 +0600

    todo mvn controller edit

--- src/main/java/com/zahid/htmx/controller/TodoController.java
+++ src/main/java/com/zahid/htmx/controller/TodoController.java
@@ -1,9 +1,17 @@
 package com.zahid.htmx.controller;
 
-import org.springframework.http.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.zahid.htmx.model.Todo;
@@ -12,11 +20,6 @@ import io.github.wimdeblauwe.htmx.spring.boot.mvc.HtmxResponse;
 import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRequest;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.log4j.Log4j2;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Log4j2
 @Controller
