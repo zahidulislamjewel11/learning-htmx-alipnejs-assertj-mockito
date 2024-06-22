7db1b83d93a6bdac5b64904fafc1becc26ccd577
digital-animal <zahidulislamssf@gmail.com>
2024-06-22 13:51:05 +0600

    custom exception message add

--- src/main/java/com/zahid/htmx/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zahid/htmx/exception/GlobalExceptionHandler.java
@@ -2,37 +2,47 @@ package com.zahid.htmx.exception;
 
 import java.util.NoSuchElementException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    private static final String CUSTOM_EXCEPTION_VIEW = "exception/custom-exception";
+    private static final String CUSTOM_EXCEPTION_TEMPLATE = "exception/custom-exception";
 
     @ExceptionHandler(value = Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleAnyException(Exception ex, Model model) {
         model.addAttribute("message", ex.getMessage());
-        return CUSTOM_EXCEPTION_VIEW;
+        model.addAttribute("statusCode", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return CUSTOM_EXCEPTION_TEMPLATE;
     }
     
     @ExceptionHandler(value = NullPointerException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleNullPointerException(NullPointerException ex, Model model) {
         model.addAttribute("message", ex.getMessage());
-        return CUSTOM_EXCEPTION_VIEW;
+        model.addAttribute("statusCode", HttpStatus.BAD_REQUEST.value());
+        return CUSTOM_EXCEPTION_TEMPLATE;
     }
     
     @ExceptionHandler(value = EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleEntityNotFoundException(EntityNotFoundException ex, Model model) {
         model.addAttribute("message", ex.getMessage());
-        return CUSTOM_EXCEPTION_VIEW;
+        model.addAttribute("statusCode", HttpStatus.NOT_FOUND.value());
+        return CUSTOM_EXCEPTION_TEMPLATE;
     }
     
     @ExceptionHandler(value = NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleNoSuchElementException(NoSuchElementException ex, Model model) {
         model.addAttribute("message", ex.getMessage());
-        return CUSTOM_EXCEPTION_VIEW;
+        model.addAttribute("statusCode", HttpStatus.NOT_FOUND.value());
+        return CUSTOM_EXCEPTION_TEMPLATE;
     }
 
 }
