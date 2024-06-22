8ced00f27d62ae9cfdb3b7fe628b72bfcf46d675
digital-animal <zahidulislamssf@gmail.com>
2024-06-23 00:16:04 +0600

    docker compose and postgres add

--- src/main/java/com/zahid/htmx/model/Todo.java
+++ src/main/java/com/zahid/htmx/model/Todo.java
@@ -1,6 +1,9 @@
 package com.zahid.htmx.model;
 
 import groovy.transform.ToString;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +14,11 @@ import lombok.Setter;
 @ToString
 @NoArgsConstructor
 @EqualsAndHashCode(of = {"id", "task", "isCompleted"})
+@Entity
+@Table(name = "todo")
 public class Todo {
 
+    @Id
     private Integer id;
     private String task;
     private Boolean isCompleted = Boolean.FALSE;
